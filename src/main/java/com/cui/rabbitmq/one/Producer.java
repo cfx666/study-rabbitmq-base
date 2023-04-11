ef19e8f71fb05d08aaa6b89dd7c06836ff56a72c
xuxie <2664481468@qq.com>
2023-04-11 13:57:43 +0800

    hello案例

---
+++ src/main/java/com/cui/rabbitmq/one/Producer.java
@@ -0,0 +1,58 @@
+package com.cui.rabbitmq.one;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 生产者
+ */
+
+public class Producer {
+
+    public static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //创建一个连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        //设置rabbitmq的ip，登录账号，密码
+        factory.setHost("192.168.137.88");
+        factory.setUsername("admin");
+        factory.setPassword("123");
+
+        //创建一个Connection
+        Connection connection = factory.newConnection();
+
+        //创建信道
+        Channel channel = connection.createChannel();
+
+        /**
+         * 生成一个队列
+         * 1.队列名称
+         * 2.队列里面的消息是否持久化 默认消息存储在内存中
+         * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
+         * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
+         * 5.其他参数
+         */
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+
+        //发送的消息
+        String message="hello world";
+
+        /**
+         * 发送一个消息
+         * 1.发送到那个交换机
+         * 2.路由的key是哪个
+         * 3.其他的参数信息
+         * 4.发送消息的消息体
+         */
+        channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+        System.out.println("消息发送完毕");
+
+
+    }
+}
